5247f67ec1f05ba711895e1c7ce48627995d6d71
mahestro <millanluiseduardo@gmail.com>
2018-10-25 20:31:52 +0200

    fix DB session

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var fs = require('fs'),
     express = require('express'),
     bodyParser = require('body-parser'),
     session = require('express-session'),
+    MongoDBStore = require('connect-mongodb-session')(session),
     cors = require('cors'),
     errorhandler = require('errorhandler'),
     mongoose = require('mongoose'),
@@ -15,9 +16,30 @@ var fs = require('fs'),
 
 var isProduction = process.env.NODE_ENV === 'production';
 var port = process.env.PORT || 3001;
+var dbConnection = isProduction ? process.env.MONGODB_URI : 'mongodb://localhost/tc-marvel';
 
 // Create global app object
 var app = express();
+
+mongoose.connect(dbConnection);
+mongoose.set('debug', true);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+var store = new MongoDBStore({
+  uri: dbConnection,
+  collection: 'sessions'
+});
+
+app.use(session({
+  secret: 'This is a secret',
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7
+  },
+  store: store,
+  resave: true,
+  saveUninitialized: true
+}));
 
 app.use(cors());
 
@@ -29,21 +51,10 @@ app.use(bodyParser.json());
 app.use(require('method-override')());
 app.use(express.static(__dirname + '/public'));
 
-app.use(session({ secret: 'tc-marvel', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false  }));
-
 if (!isProduction) {
   app.use(errorhandler());
 }
 
-if(isProduction){
-  mongoose.connect(process.env.MONGODB_URI);
-} else {
-  mongoose.connect('mongodb://localhost/tc-marvel');
-  mongoose.set('debug', true);
-
-  var db = mongoose.connection;
-  db.on('error', console.error.bind(console, 'connection error:'));
-}
 app.use(require('./routes'));
 
 /// catch 404 and forward to error handler
@@ -85,79 +96,34 @@ var server = app.listen(port, function(){
   console.log('Listening on port ' + server.address().port);
 });
 
-
-// const { Builder, By, until } = require('selenium-webdriver');
-// const capabilities = {
-//     'browserName': 'chrome',
-//     'chromeOptions': {
-//       'args' : ["--disable-plugins"]
+// const { Builder, By, Key, until, Capabilities } = require('selenium-webdriver');
+// var chromeCapabilities = Capabilities.chrome();
+// var chromeOptions = {
+//     'args': ['--test-type', '--start-maximized', '--disable-plugins'],
+//     'prefs': {
+//       'download.default_directory': '/Users/**/app/output/'
 //     }
 // };
-// const driver = new Builder().withCapabilities(capabilities).build();
+// chromeCapabilities.set('chromeOptions', chromeOptions);
 
-// driver.get('http://www.google.com')
-//   .then(() => {
-//     driver.getTitle().then(function(title) {
+// (async function google() {
+//   let driver = await new Builder().forBrowser('chrome')
+//     .withCapabilities(chromeCapabilities)
+//     .build();
+
+//   try {
+//     await driver.get('http://www.google.com');
+//     await driver.getTitle().then(function(title) {
 //       console.log('Old page title is: ' + title);
 //     })
-//   })
-
-// var element = driver.findElement(By.name('q'));
-// element.sendKeys('Cheese!')
-//   .then(() => {
-//     element.submit()
-//       .then(() => {
-//         driver.getTitle()
-//           .then(function(title) {
-//             console.log('New age title is: ' + title);
-//             driver.close()
-//               .catch(err => {console.log(err)});
-//             driver.quit()
-//             .catch(err => {console.log(err)});
-//           })
-//           .catch(err => {
-//             console.log(err);
-//           });
-//       })
-//       .catch(err => {
-//         console.log(err);
-//       });
-//   })
-//   .catch(err => {
-//     console.log(err);
-//   });
-
-
-
-
-const { Builder, By, Key, until, Capabilities } = require('selenium-webdriver');
-var chromeCapabilities = Capabilities.chrome();
-var chromeOptions = {
-    'args': ['--test-type', '--start-maximized', '--disable-plugins'],
-    'prefs': {
-      'download.default_directory': '/Users/**/app/output/'
-    }
-};
-chromeCapabilities.set('chromeOptions', chromeOptions);
-
-(async function google() {
-  let driver = await new Builder().forBrowser('chrome')
-    .withCapabilities(chromeCapabilities)
-    .build();
-
-  try {
-    await driver.get('http://www.google.com');
-    await driver.getTitle().then(function(title) {
-      console.log('Old page title is: ' + title);
-    })
-    // await driver.wait(until.elementLocated(By.id('lst-ib')), 20000);
-    const element = await driver.findElement(By.name('q'));
-    await element.sendKeys('Cheese!');
-    await element.submit();
-    await driver.getTitle().then(function(title) {
-      console.log('New age title is: ' + title);
-    });
-  } finally {
-    await driver.quit();
-  }
-})();
+//     // await driver.wait(until.elementLocated(By.id('lst-ib')), 20000);
+//     const element = await driver.findElement(By.name('q'));
+//     await element.sendKeys('Cheese!');
+//     await element.submit();
+//     await driver.getTitle().then(function(title) {
+//       console.log('New age title is: ' + title);
+//     });
+//   } finally {
+//     await driver.quit();
+//   }
+// })();
